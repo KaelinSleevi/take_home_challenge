9ae0f28fd3d70059ed2e1c472d915cf87f06b2ef
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2023-01-11 17:39:46 -0700

    feat: add tea and tea_subscription models to get subscription relationship tests to pass.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,7 @@
 class Subscription < ApplicationRecord
  belongs_to :customer
+ has_many :tea_subscriptions
+ has_many :teas, through: :tea_subscriptions
 
  validates_presence_of :title
  validates_presence_of :price
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   describe 'relationships' do
     it {should belong_to :customer}
+    it { should have_many :tea_subscriptions}
+    it { should have_many(:teas).through(:tea_subscriptions)}
   end
 
   describe 'validations' do
