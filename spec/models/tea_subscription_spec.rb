4b1a47ca62fd87e3e3b0dc1b5369fae151ef4dd3
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2023-01-11 17:50:27 -0700

    test: add validations tests to tea_subscriptions.

--- spec/models/tea_subscription_spec.rb
+++ spec/models/tea_subscription_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe TeaSubscription, type: :model do
   end
 
   describe 'validations' do
-
+   it {should validate_presence_of(:tea_id)}
+   it {should validate_presence_of(:subscription_id)}
   end
 end
